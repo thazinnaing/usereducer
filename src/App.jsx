e95bad9eca2137b7697beffff97f42272cfd5c3d
thazin naing <thazinaingmm@gmail.com>
2023-07-20 17:45:01 +0630

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,59 @@
 import React from "react";
 import { useReducer } from "react";
 
-const reducer=(state, action)=>{
-  if(action.type === 'increment-age'){
-    return {
-      age: state.age+1
+ const reducer=(state, action)=>{
+  switch(action.type){
+    case 'increment-age':{
+      return{
+        name: state.name,
+        age: state.age+1
+      };
+  }
+    case 'decrement-age':{
+      return{
+        name: state.name,
+        age: state.age-1
+      };
+    }
+    case 'changed-name':{
+      return{
+        name: action.nextName,
+        age: state.age
+      }
     }
   }
-  else{
-    return{
-      age: state.age-1
-    }
+
   }
   
-}
-
 const App=()=>{
-  const [state, dispatch]=useReducer(reducer, {age: 4});
+  const [state, dispatch]=useReducer(reducer, {age: 4, name:"thazin"});
 
+  const handleInput=(e)=>{
+    dispatch({
+      type:"changed-name",
+      nextName: e.target.value
+    });
+  }
 
   return(
     <>
-    <button onClick={()=>{
-      dispatch({type: 'increment-age'})
-    }}>
+      <input
+        value={state.name}
+        onChange={handleInput}
+      />
+    <button 
+      onClick={()=>{dispatch({type: 'increment-age'})}}
+      >
       Increment age
-    </button>
-    <button onClick={()=>{
-      dispatch({type: 'decrement-age'})
-    }}>
-      Decrement age
-    </button>
-    <p>Hello! Your age is {state.age}</p>
-    
-    
+      </button>
+      <button 
+       onClick={()=>{dispatch({type: 'decrement-age'})}}
+      >
+        Decrement age
+      </button>
+      <p>Hello! {state.name} Your age is {state.age}</p>
     
     </>
-
-
-
-
   )
 }
 
